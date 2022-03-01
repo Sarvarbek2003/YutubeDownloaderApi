a4df016004850b7c01c9a34c11e3dfa66df58f0f
Sarvarbek <sarvarbekodilov070603@gmail.com>
2022-03-01 22:22:05 +0500

    changed

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('path')
 const ytdl = require('ytdl-core');
 const express = require('express')
 const app = express()
-const cors = require('cors')
+const cors = require('cors');
+const { isError } = require('util');
 const PORT = process.env.PORT || 3000
 
 
@@ -15,26 +16,30 @@ app.use(cors({
 app.use(express.json())
 
 app.get('/videos', async(req, res) => {
-    const { link } = req.query
-    if( !link ) return res.send('hech narsa bo`midi')
-    let fech = true 
-    let response = await ytdl(link)
-            .pipe(a = await fs.createWriteStream(path.join(process.cwd(), 'files', link+'.mp4')));
-    setInterval(async() => {
-        if(fech){
-            if(response.closed == true){
-                fech = false
-                let obj = {
-                    videoId: link,
-                    url: path.join(process.cwd(), 'files', link+'.mp4')
+    try{
+        const { link } = req.query
+        if( !link ) return res.send('hech narsa bo`midi')
+        let fech = true 
+        let response = await ytdl(link)
+                .pipe(a = await fs.createWriteStream(path.join(process.cwd(), 'files', link+'.mp4')));
+        setInterval(async() => {
+            if(fech){
+                if(response.closed == true){
+                    fech = false
+                    let obj = {
+                        videoId: link,
+                        url: path.join(process.cwd(), 'files', link+'.mp4')
+                    }
+                    res.json(obj)
+                    setTimeout(async() => {
+                        await fs.unlinkSync(path.join(process.cwd(),'files', link+'.mp4' ))
+                    }, 10000);
                 }
-                res.json(obj)
-                setTimeout(async() => {
-                    await fs.unlinkSync(path.join(process.cwd(),'files', link+'.mp4' ))
-                }, 10000);
             }
-        }
-    }, 500);
+        }, 500);
+    }catch(err){
+        console.log(err)
+    }
 })
   
 
