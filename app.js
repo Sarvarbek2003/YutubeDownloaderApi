98e6e60242ba731de1885cdc10cc10a5f612d701
Sarvarbek <sarvarbekodilov070603@gmail.com>
2022-03-01 19:19:00 +0500

    changed

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const path = require('path')
 const ytdl = require('ytdl-core');
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = process.env.PORT || 3000
 
+
+app.use(cors())
 app.use(express.json())
+
 app.get('/videos', async(req, res) => {
     const { link } = req.query
-    if( !link ) return
+    if( !link ) res.send('hech narsa bo`midi')
     let fech = true 
     let response = await ytdl(link)
             .pipe(a = await fs.createWriteStream(path.join(process.cwd(), 'files', link+'.mp4')));
